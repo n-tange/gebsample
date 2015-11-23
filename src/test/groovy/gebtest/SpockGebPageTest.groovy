f1d591fe8dc95812de0983add7e6803b8d85e365
n-tange <ntange0@gmail.com>
2015-11-24 00:35:32 +0900

    Spockを使用したテストケースを追加

---
+++ src/test/groovy/gebtest/SpockGebPageTest.groovy
@@ -0,0 +1,25 @@
+package gebtest
+
+import geb.spock.GebReportingSpec
+import gebtest.page.*
+
+class SpockGebPageTest extends GebReportingSpec{
+    def "Spockを使用したテストケース"(){
+      setup:
+        reportGroup 'gebish.org_3'
+      
+      when:
+        to TopPage
+      then:
+        waitFor{ at TopPage }
+        assert title == "Geb - Very Groovy Browser Automation"
+        report '01_Geb_Top'
+      
+      when:
+        sidemenu.idx2.click()
+      then:
+        waitFor{ at JqueryLikePage }
+        assert sidemenu.idx2.parent().hasClass("selected")
+        report '02_Geb_Navigating_Content'
+    }
+}
