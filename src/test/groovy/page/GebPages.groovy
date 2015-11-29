de59ab472371e3a135d270623c40a45792bbab48
Yu Yamaguchi <sansou.android.yamaguchi@gmail.com>
2015-11-29 18:02:23 +0900

    sansou-dev.jpのさくらVPサーバ上にあるTracで動作するようにSpockGetPageTest.groovyを作成しました。

--- src/test/groovy/page/GebPages.groovy
+++ src/test/groovy/page/GebPages.groovy
@@ -1,25 +1,19 @@
+package gebtest.page
+
 import geb.Page
 import geb.Module
 
 class TopPage extends Page{
-  static url = "http://www.gebish.org/"
-  static at = { $("#main h1", 0).text() == "What is it?"}
-  static content = {
-    //sidemenu_2{$("#sidebar .sidemenu a", text: "jQuery-like API")}
-    sidemenu { module SidemenuModule }
-  }
+  static url = "http://www.sansou-dev.jp/trac/sansou-dev"
+  static at = { $("#mainnav ul li a").text() == "Wiki"}
+  static content = {}
 }
 
-class JqueryLikePage extends Page{
-  static at = { $("#main h1")*.text() == ["Navigating Content", "Form Control Shortcuts"] }
+class TicketRegPage extends Page{
+  static at = { $("#mainnav a", href: endsWith("newticket")).parent().hasClass("active") }
   static content = {
-    //sidemenu_2{$("#sidebar .sidemenu a", text: "jQuery-like API")}
-    sidemenu { module SidemenuModule }
-  }
-}
-
-class SidemenuModule extends Module{
-  static content = {
-    idx2{$("#sidebar .sidemenu a", text: "jQuery-like API")}
+      txtSummary {$("#field-summary")}
+      txtReporter {$("#field-reporter")}
+      txtDescription {$("#field-description")}
   }
 }
